83045f82f7db5444d532d67cabb8bf9a314772d2
Aaron Wilson <124102001+aaronthebald@users.noreply.github.com>
2023-04-08 14:56:37 -0400

    Pushing

--- Distance/Core/Views/DistanceHomeView.swift
+++ Distance/Core/Views/DistanceHomeView.swift
@@ -16,6 +16,7 @@ struct DistanceHomeView: View {
             HStack {
                 Button {
                     vm.timeFrame = .thisMonth
+                    print("I made a change")
                 } label: {
                     Text("This month")
                 }
